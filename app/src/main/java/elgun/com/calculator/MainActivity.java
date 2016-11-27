58761f51bb63f632711adac9676b72f6fcc86945
Elgun <emajidov2018>
2016-11-27 23:23:35 +0400

    Calculator with new design and less code

--- app/src/main/java/elgun/com/calculator/MainActivity.java
+++ app/src/main/java/elgun/com/calculator/MainActivity.java
@@ -1,232 +1,83 @@
 package elgun.com.calculator;
-import android.net.ParseException;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+public class MainActivity extends Activity {
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button  zero, one, two, three, four, five, six,
-            seven, eight, nine, add, sub, div,
-            mul, point, clear, equal, in, out, del;
-
-    TextView display ;
-    String value1= "", value2 ="";
-    double vOne , vTwo, result;
-    String op = "";
-    String buffer = "";
-    String inBuffer = "";
-    // boolean mAddition , mSubtract ,mMultiplication ,mDivision ;
-
+   TextView display ;
+    private String v1= "", v2 ="", op = "", buffer = "", inBuffer = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        zero = (Button) findViewById(R.id.zero);
-        one = (Button) findViewById(R.id.one);
-        two = (Button) findViewById(R.id.two);
-        three = (Button) findViewById(R.id.three);
-        four = (Button) findViewById(R.id.four);
-        five = (Button) findViewById(R.id.five);
-        six = (Button) findViewById(R.id.six);
-        seven = (Button) findViewById(R.id.seven);
-        eight = (Button) findViewById(R.id.eight);
-        nine = (Button) findViewById(R.id.nine);
-        point = (Button) findViewById(R.id.point);
-        add = (Button) findViewById(R.id.plus);
-        sub = (Button) findViewById(R.id.minus);
-        mul = (Button) findViewById(R.id.product);
-        div = (Button) findViewById(R.id.divide);
-        clear = (Button) findViewById(R.id.clear);
-        equal = (Button) findViewById(R.id.equals);
-        in = (Button) findViewById(R.id.in_button);
-        out = (Button) findViewById(R.id.out_button);
-        del = (Button) findViewById(R.id.del);
-        display = (TextView) findViewById(R.id.display);
-
-        one.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value1  = value1+"1";
-                display.setText(buffer + value1);
-
-            }
-        });
-
-        two.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value1 = value1 +"2";
-                display.setText(buffer+value1);
-            }
-        });
-
-        three.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value1 = value1 +"3";
-                display.setText(buffer+value1);
-            }
-        });
-
-        four.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value1 = value1 +"4";
-                display.setText(buffer+value1);
-            }
-        });
-
-        five.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value1 = value1 +"5";
-                display.setText(buffer+value1);
-            }
-        });
-
-        six.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value1 = value1 +"6";
-                display.setText(buffer+value1);
-            }
-        });
-
-        seven.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value1 = value1 +"7";
-                display.setText(buffer+value1);
-            }
-        });
-
-        eight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value1 = value1 +"8";
-                display.setText(buffer+value1);
-            }
-        });
-
-        nine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value1 = value1 +"9";
-                display.setText(buffer+value1);
-            }
-        });
-
-        zero.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value1 = value1 +"0";
-                display.setText(buffer+value1);
-            }
-        });
-        point.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(!value1.contains(".")) value1 =  value1 + ".";
-                display.setText(buffer+ value1);
-            }
-        });
-        clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value1 = "";
-                value2 = "";
-                buffer = "";
-                display.setText("");
-            }
-        });
-        del.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(value1.length()!= 0) {
-                    int j = value1.length();
-                    value1 = value1.substring(0, j - 1);
-                    display.setText(value1);
-                }
-            }
-        });
-        in.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    vTwo = Double.parseDouble(value1);
-                }
-                catch(Exception e) {
-                }
-
-                inBuffer =  String.valueOf(vTwo+vOne);
-                display.setText("");
-            }
-        });
-        out.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                value1 = inBuffer;
-                display.setText(value1);
-            }
-        });
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                display.setText(value1+"+");
-                buffer =  (String)display.getText();
-                try {
-                    vOne = Double.parseDouble(value1);
-                }
-                catch(Exception e) {
-                }
-                value1 = "";
-                op = "add";
-            }
-        });
-        sub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                display.setText(value1+"-");
-                buffer = (String)display.getText();
-                try {
-                    vOne = Double.parseDouble(value1);
-                }
-                catch (Exception e) {
-                }
-                value1 = "";
-                op ="sub";
-            }
-        });
-
-
-
-        equal.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                display.setText(buffer+value1);
-                try {
-                    vTwo = Double.parseDouble(value1);
-                }
-                catch(Exception e) {
-                }
-                if(op.equals("add")){
-                    result = vOne+vTwo;
-                    op ="";
-                }
-                if(op.equals("sub")){
-                    result = vOne-vTwo;
-                    op="";
-                }
-                value1 =  String.valueOf(result);
-                display.setText(value1)      ;
-            }
-        });
+        display = (TextView)findViewById(R.id.display);
+    }
+    public void clear(View v){
+      Button button = (Button)v;
+      v1 = "";
+        display.setText("");
+    }
+    public void number(View v){
+        Button button = (Button)v;
+        if(!button.getText().toString().equalsIgnoreCase("del")) {
+            v1 += button.getText().toString();
+            display.setText(v1);
+        }
+        else if(!v1.equals("")){
+            v1 = v1.substring(0, v1.length() -1);
+            display.setText(v1);
+        }
+    }
+    public void point(View v){
 
     }
+    public void sign(View v){
+        op="";
+        Button button = (Button)v;
+        op = button.getText().toString();
+        v1 += op;
+        display.setText(v1);
+    }
+    public void in(View v){
+        Button button = (Button)v;
+    }
+
+    public void out(View v){
+        Button button = (Button)v;
+    }
+    public void calculate(View v) {
+        double result = 0;
+        Button button = (Button) v;
+        String[] num = display.getText().toString().split("(?<=[-+*/])|(?=[-+*/])");
+        for (int i = 0; i < num.length; i++) {
+
+            if (num[i].equals("×")) {
+                result = Double.parseDouble(num[i+1])*Double.parseDouble(num[i-1]);
+
+            }
+            if (num[i].equals("÷")) {
+                result = Double.parseDouble(num[i+1])/Double.parseDouble(num[i-1]);
+
+
+            }
+            if (num[i].equals("+")) {
+                result = Double.parseDouble(num[i+1]);
+            }
+            if (num[i].equals("-")) {
+                result = Double.parseDouble(num[i+1]);
+            }
+        }
+        display.setText(display.getText() + "\n");
+        display.setTextSize(20); display.setTextColor(Color.RED);
+        display.setText(String.valueOf(result));
+        result = 0;
+    }
 }
+/* REFERENCES */
+/*
+Splitting of strings. from StackOverFlow.
+http://stackoverflow.com/questions/13525024/how-to-split-a-mathematical-expression-on-operators-as-delimiters-while-keeping
+ */
