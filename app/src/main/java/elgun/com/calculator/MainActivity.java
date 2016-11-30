54e28e1c643ea94ff9b828d19fc002952ec24cd5
Elgun <emajidov2018>
2016-11-30 14:41:36 +0400

    Fully Working App

--- app/src/main/java/elgun/com/calculator/MainActivity.java
+++ app/src/main/java/elgun/com/calculator/MainActivity.java
@@ -1,79 +1,113 @@
 package elgun.com.calculator;
-import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-public class MainActivity extends Activity {
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
 
-   TextView display ;
-    private String v1= "", v2 ="", op = "", buffer = "", inBuffer = "";
+    TextView display;
+    private String v1 = "", operand = "", memory = "";
+    private boolean isResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        display = (TextView)findViewById(R.id.display);
+        display = (TextView) findViewById(R.id.display);
     }
-    public void clear(View v){
-      Button button = (Button)v;
-      v1 = "";
+    public void clear(View v) {
+        v1 = "";
         display.setText("");
     }
-    public void number(View v){
-        Button button = (Button)v;
-        if(!button.getText().toString().equalsIgnoreCase("del")) {
-            v1 += button.getText().toString();
-            display.setText(v1);
+    public void number(View v) {
+        if(isResult) {
+            v1 = "";
+            isResult = false;
         }
-        else if(!v1.equals("")){
-            v1 = v1.substring(0, v1.length() -1);
-            display.setText(v1);
-        }
+          Button button = (Button) v;
+            if (!button.getText().toString().equalsIgnoreCase("del")) {
+                v1 += button.getText().toString();
+                display.setText(v1);
+            } else if (!v1.equals("")) {
+                v1 = v1.substring(0, v1.length() - 1);
+                display.setText(v1);
+            }
     }
     public void point(View v){
-
-    }
-    public void sign(View v){
-        op="";
-        Button button = (Button)v;
-        op = button.getText().toString();
-        v1 += op;
+        String[] a = v1.split("(?<=[-+])|(?=[-+])");
+       if(!a[a.length-1].contains("."))v1 += ".";
         display.setText(v1);
     }
-    public void in(View v){
+    public void sign(View v) {
+        isResult = false;
         Button button = (Button)v;
-    }
-
-    public void out(View v){
-        Button button = (Button)v;
-    }
-    public void calculate(View v) {
-        double result = 0;
-        Button button = (Button) v;
-        String[] num = display.getText().toString().split("(?<=[-+*/])|(?=[-+*/])");
-        for (int i = 0; i < num.length; i++) {
-
-            if (num[i].equals("×")) {
-                result = Double.parseDouble(num[i+1])*Double.parseDouble(num[i-1]);
-
-            }
-            if (num[i].equals("÷")) {
-                result = Double.parseDouble(num[i+1])/Double.parseDouble(num[i-1]);
-
-
-            }
-            if (num[i].equals("+")) {
-                result = Double.parseDouble(num[i+1]);
-            }
-            if (num[i].equals("-")) {
-                result = Double.parseDouble(num[i+1]);
+        operand = button.getText().toString();
+        if(!v1.isEmpty()) {
+            if ((v1.substring(v1.length() - 1, v1.length()).equals("+")) || (v1.substring(v1.length() - 1, v1.length()).equals("-")) ||
+                    (v1.substring(v1.length() - 1, v1.length()).equals("×")) || (v1.substring(v1.length() - 1, v1.length()).equals("÷"))) {
+                v1 = v1.substring(0, v1.length() - 1);
             }
         }
-        display.setText(display.getText() + "\n");
-        display.setTextSize(20); display.setTextColor(Color.RED);
-        display.setText(String.valueOf(result));
-        result = 0;
+        if(v1.isEmpty()&&(operand.equals("×")||operand.equals("÷"))) operand = "";
+
+            v1 += operand;
+            display.setText(v1);
+    }
+    public void in(View v) {
+        try{
+            memory = String.valueOf((calculate(v1)));
+            display.setText("");
+            v1 = "";
+        }
+        catch (Exception e) {
+            Toast.makeText(MainActivity.this, "Please do not do this!", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void out(View v) {
+        v1 += memory;
+        display.setText(v1);
+    }
+    public void calculate(View v) {
+       try {
+            double result = calculate(display.getText().toString());
+
+            display.setText(display.getText() + "\n" + String.valueOf(result));
+            v1 = String.valueOf(result);
+            isResult = true;
+        }
+        catch(Exception e) {
+            Toast.makeText(this.getApplicationContext(),"Please, do not do that!",Toast.LENGTH_SHORT).show();
+            isResult = false;
+        }
+    }
+
+    public static  double calculate(String ex){
+        double res = 0;
+        String[] a = ex.split("(?<=[-+])|(?=[-+])");
+        for (int i = 0; i < a.length; i++) {
+            if (a[i].equals(""))
+                a[i] = "0";
+            if (a[i].contains("÷") || a[i].contains("×")) {
+                String[] mul = a[i].split("(?<=[×÷])|(?=[×÷])");
+                double r = Double.parseDouble(mul[0]);
+                for (int j = 0; j < mul.length; j++) {
+                    if (mul[j].equals("×"))
+                        r = r * Double.parseDouble(mul[j + 1]);
+                    if (mul[j].equals("÷"))
+                        r = r / Double.parseDouble(mul[j + 1]);
+                }
+                a[i] = String.valueOf(r);
+            }
+        }
+        res = Double.parseDouble(a[0]);
+        for (int i = 0 ; i < a.length; i++) {
+            if (a[i].equals("+"))
+                res += Double.parseDouble(a[i + 1]);
+            if (a[i].equals("-"))
+                res -= Double.parseDouble(a[i + 1]);
+        }
+       return res;
     }
 }
 /* REFERENCES */
